3b85ae3a857e4bb0f187ed8cdcacef4572665826
Bezaka <166315440+Bezaka@users.noreply.github.com>
2024-04-07 16:26:10 +0100

    Add files via upload

---
+++ Site_artiste/ollmeck.js
@@ -0,0 +1,6 @@
+                   // Animation
+
+window.addEventListener('scroll', function(){
+  const header =document.querySelector('header');
+  header.classList.toggle("sticky",window.scrollY > 0);
+})
